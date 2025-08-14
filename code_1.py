1c07ece454a7c1c1324ffdf7f2851037156efca7
Sharjeel <sharjeel6392@gmail.com>
2025-08-14 11:12:04 +0530

    Second version of data saved

--- code_1.py
+++ code_1.py
@@ -8,6 +8,15 @@ data = {
 }
 df = pd.DataFrame(data)
 
+# uncomment the following lines to for different versions of the file
+## Adding a new row to df for Version 2
+new_row = {'Name': 'David', 'Age': 28, 'City': 'San Francisco'}
+df.loc[len(df.index)] = new_row
+
+## Adding a new row to df for version 3
+# new_row = {'Name': 'Eve', 'Age': 22, 'City': 'Seattle'}
+# df.loc[len(df.index)] = new_row
+
 # Ensure the directory exists at the root level
 output_dir = 'data'
 os.makedirs(output_dir, exist_ok=True)
@@ -18,12 +27,3 @@ file_path = os.path.join(output_dir, 'sample_data.csv')
 df.to_csv(file_path, index=False)
 # Print the path where the file is saved
 print(f"DataFrame saved to {file_path}")
-
-# uncomment the following lines to for different versions of the file
-## Adding a new row to df for Version 2
-# new_row = {'Name': 'David', 'Age': 28, 'City': 'San Francisco'}
-# df.loc[len(df.index)] = new_row
-
-## Adding a new row to df for version 3
-# new_row = {'Name': 'Eve', 'Age': 22, 'City': 'Seattle'}
-# df.loc[len(df.index)] = new_row
